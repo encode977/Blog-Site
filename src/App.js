1bbbf9eb2d467bd6172058a14676ae270aceeadf
Lokindra Dangi <pachar.com@gmail.com>
2021-09-13 12:21:48 +0545

    header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        <h2>Blog Site</h2>
+        <Header />
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Header.scss";
+import Logo from "../assets/Logo.svg";
 
 function Header() {
   return (
-    <header>
-      <h3>Header Components</h3>
+    <header className="header">
+      <div className="header__left">
+        <img src={Logo} alt="Unifeed" />
+      </div>
+      <div className="header__right">
+        <ul>
+          <li>Home</li>
+          <li>Features</li>
+          <li>Pricing</li>
+        </ul>
+        <button className="header__btn">Subscribe</button>
+      </div>
     </header>
   );
 }
